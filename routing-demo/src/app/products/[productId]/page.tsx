6eb01907f60e6dea9ba7f95ca07c5ab7e9da14c1
Vishwas <vishwas.g5@gmail.com>
2024-11-09 10:38:23 +0530

    async params fix

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,15 +1,16 @@
 import { Metadata } from "next";
 
 type Props = {
-  params: { productId: string };
+  params: Promise<{ productId: string }>;
 };
 
 export const generateMetadata = async ({
   params,
 }: Props): Promise<Metadata> => {
+  const productId = (await params).productId;
   const title = await new Promise((resolve) => {
     setTimeout(() => {
-      resolve(`iPhone ${params.productId}`);
+      resolve(`iPhone ${productId}`);
     }, 100);
   });
   return {
