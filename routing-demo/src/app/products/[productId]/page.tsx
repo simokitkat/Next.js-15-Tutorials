b5e42009fe4e0f0bfdbf169466ac4c912da26880
Vishwas <vishwas.g5@gmail.com>
2024-11-09 09:53:35 +0530

    Routing metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,23 @@
-export default async function ProductDetails({
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: Promise<{ productId: string }>;
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product - ${title}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Details about product {productId}</h1>;
 }
