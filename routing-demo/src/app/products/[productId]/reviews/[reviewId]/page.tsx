6f4b92604021e9b33f120d02f7c91e5dd6c1f294
Vishwas <vishwas.g5@gmail.com>
2024-11-10 10:52:35 +0530

    Errors in layouts

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,18 +1,18 @@
 import { notFound } from "next/navigation";
 
-function getRandomInt(count: number) {
-  return Math.floor(Math.random() * count);
-}
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
 
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
-  const random = getRandomInt(2);
-  if (random === 1) {
-    throw new Error("Error loading review");
-  }
+  // const random = getRandomInt(2);
+  // if (random === 1) {
+  //   throw new Error("Error loading review");
+  // }
 
   const { productId, reviewId } = await params;
 
