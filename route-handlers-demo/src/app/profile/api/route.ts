34de1b9dcd675e5e0b4c18313441b04d1f40c18a
Vishwas <vishwas.g5@gmail.com>
2024-11-12 08:31:57 +0530

    Cookies in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,6 @@
 import { type NextRequest } from "next/server";
 import { headers } from "next/headers";
+import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
@@ -8,9 +9,17 @@ export async function GET(request: NextRequest) {
   const headersList = await headers();
   console.log(headersList.get("Authorization"));
 
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"));
+
   return new Response("<h1>Profile API data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": `theme=dark`,
     },
   });
 }
