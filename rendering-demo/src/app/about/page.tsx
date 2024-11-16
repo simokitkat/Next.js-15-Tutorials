c8e36637e421f555fa94646f4833fa3d749bce52
Vishwas <vishwas.g5@gmail.com>
2024-11-16 13:02:36 +0530

    generateStaticParams

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
   console.log("About server component");
   return <h1>About page {new Date().toLocaleTimeString()}</h1>;
 }
