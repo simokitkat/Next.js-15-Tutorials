f87c2875091f5c993c9e83486fe3a81fad1aba6b
Vishwas <vishwas.g5@gmail.com>
2024-11-18 07:10:40 +0530

    Loading page

--- data-fetching-demo/src/app/users-server/page.tsx
+++ data-fetching-demo/src/app/users-server/page.tsx
@@ -7,6 +7,8 @@ type User = {
 };
 
 export default async function UsersServer() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await response.json();
   console.log(users);
