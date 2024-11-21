7b3dace501091efe914801743f00d2e163bfab7e
Vishwas <vishwas.g5@gmail.com>
2024-11-21 18:16:13 +0530

    Delete server action

--- data-fetching-demo/src/actions/products.ts
+++ data-fetching-demo/src/actions/products.ts
@@ -1,7 +1,8 @@
 "use server";
 
-import { addProduct, updateProduct } from "@/prisma-db";
+import { addProduct, updateProduct, deleteProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export type Errors = {
   title?: string;
@@ -69,4 +70,9 @@ export async function editProduct(
 
   await updateProduct(id, title, parseInt(price), description);
   redirect("/products-db");
+}
+
+export async function removeProduct(id: number) {
+  await deleteProduct(id);
+  revalidatePath("/products-db");
 }
