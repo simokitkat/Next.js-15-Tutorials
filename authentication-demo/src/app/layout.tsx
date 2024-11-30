66944fa4dcecee347a3a111702d3e0920e6404db
Vishwas <vishwas.g5@gmail.com>
2024-11-30 11:53:53 +0530

    Sign in and sign out

--- authentication-demo/src/app/layout.tsx
+++ authentication-demo/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Navigation } from "@/components/navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Navigation />
           {children}
         </body>
       </html>
