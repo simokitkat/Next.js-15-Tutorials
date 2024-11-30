4fc255221e11b3a4dab81d70f502bf619115a314
Vishwas <vishwas.g5@gmail.com>
2024-11-30 14:55:56 +0530

    RBAC

--- authentication-demo/src/middleware.ts
+++ authentication-demo/src/middleware.ts
@@ -1,10 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // const isProtectedRoute = createRouteMatcher(["/user-profile"]);
 const isPublicRoute = createRouteMatcher(["/"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
+
+  if (
+    isAdminRoute(req) &&
+    (await auth()).sessionClaims?.metadata?.role !== "admin"
+  ) {
+    const url = new URL("/", req.url);
+    return NextResponse.redirect(url);
+  }
 
   if (!userId && !isPublicRoute(req)) {
     // Add custom logic to run before redirecting
